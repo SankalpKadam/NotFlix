c704b4ba6d7f814df732c3ff66b92d726f017fd2
SankalpKadam <sankalptp07@gmail.com>
2024-01-13 04:43:40 -0600

    implemented mobile menu in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,14 @@
+import { useCallback, useState } from "react";
+import MobileMenu from "./MobileMenu";
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
+
+    //
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const showMenu = useCallback(() => {
+        setShowMobileMenu((showMobileMenu) => !showMobileMenu)
+    }, []);
     return (
         <nav className="w-full fixed z-40">
             <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
@@ -12,11 +20,29 @@ const Navbar = () => {
                     <NavbarItem label="New & Popular" />
                     <NavbarItem label="My List" />
                 </div>
-                <div className="lg:hidden flex-row flex items-center gap-2 ml-8 cursor-pointer relative">
+                <div onClick={showMenu}
+                    className="lg:hidden flex-row flex items-center gap-2 ml-8 cursor-pointer relative">
                     <p className="text-white text-sm">
                         Browse
                     </p>
-                    {/* insert and icon classname="text-white transition"*/}
+                    {/* insert chevron down and icon classname="text-white transition"*/}
+                    <MobileMenu visible={showMobileMenu} />
+                </div>
+                <div className="flex flex-row ml-auto gap-7 items-center">
+                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+                        Search
+                        {/* insert icon */}
+                    </div>
+                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+                        Bell
+                        {/* insert icon */}
+                    </div>
+                    <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+                        <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                            <img src="/images/default-blue.png" alt="" />
+                        </div>
+                        {/* insert chevron down and icon*/}
+                    </div>
                 </div>
             </div>
 
