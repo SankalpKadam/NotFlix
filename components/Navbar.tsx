0609b4877e853c77996ebed5eafc63ca36fb1eac
SankalpKadam <sankalptp07@gmail.com>
2024-01-13 04:28:20 -0600

    Working on making navbar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,25 @@
+import NavbarItem from "./NavbarItem";
+
 const Navbar = () => {
-    return(
+    return (
         <nav className="w-full fixed z-40">
             <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
-<img src="/images/logo.png" alt="Logo" className="h-4 lg:h-7" />
-         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-
-         </div>
+                <img src="/images/logo.png" alt="Logo" className="h-4 lg:h-7" />
+                <div className="flex-row ml-8 gap-7 hidden lg:flex">
+                    <NavbarItem label="Home" />
+                    <NavbarItem label="Movies" />
+                    <NavbarItem label="Series" />
+                    <NavbarItem label="New & Popular" />
+                    <NavbarItem label="My List" />
+                </div>
+                <div className="lg:hidden flex-row flex items-center gap-2 ml-8 cursor-pointer relative">
+                    <p className="text-white text-sm">
+                        Browse
+                    </p>
+                    {/* insert and icon classname="text-white transition"*/}
+                </div>
             </div>
-        
+
         </nav>
     )
 }
