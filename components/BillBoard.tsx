9d512ece0adf69a2505c154cc4815008def37265
SankalpKadam <sankalptp07@gmail.com>
2024-01-13 16:59:32 -0600

    Completed the play button

--- components/BillBoard.tsx
+++ components/BillBoard.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import PlayButton from './PlayButton';
 
 const BillBoard = () => {
     const data = {
+        "id":"1",
         "title": "Sintel",
         "description": "A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.",
         "videoUrl": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
@@ -25,6 +27,7 @@ const BillBoard = () => {
                     {data?.description}
                 </p>
                 <div className='flex flex-row items-center mt-3 md:mt-4 gap-3'>
+                    <PlayButton movieId={data?.id}/>
                     <button className='bg-white
                     text-white bg-opacity-30 rounded py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg: text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition'>{/*Add more info icon mr-1 */}More Info</button>
                 </div>
---
+++ components/PlayButton.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+interface PlayButtonProps {
+    movieId: string;
+}
+const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+    const router = useRouter();
+    return (
+        <div className='bg-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-neutral-300 transition' onClick={() =>
+            router.push('/watch')
+        }>
+            Play
+            {/*inster play icon mr-1 */}
+        </div>
+    )
+}
+
+export default PlayButton;
