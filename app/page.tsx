dcbd24544b7475751513325d5b89311c63b89d6c
SankalpKadam <sankalptp07@gmail.com>
2024-01-13 15:34:28 -0600

    Created the billboard component to play movies when logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import BillBoard from "@/components/BillBoard"
 import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
+      <BillBoard/>
     </main>
   )
 }
---
+++ components/BillBoard.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const BillBoard = () => {
+    const data = {
+        "title": "Sintel",
+        "description": "A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.",
+        "videoUrl": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+        "thumbnailUrl": "http://uhdtv.io/wp-content/uploads/2020/10/Sintel-3.jpg",
+        "genre": "Adventure",
+        "duration": "15 minutes"
+    }
+    return (
+        <div className='relative h-[56.25vw]'>
+            <video
+                className='w-full h-[56.25vw] object-cover brightness-[60%]'
+                autoPlay
+                muted
+                loop
+                src={data?.videoUrl} poster={data?.thumbnailUrl}
+            ></video>
+            <div className='absolute top-[30%] md:top-[40%] ml-4 md:ml-16'>
+                <p className='text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl'>{data?.title}</p>
+                <p className='text-white text-[8px] md:text-lg mt-3
+                md:mt-8 md:w-[80%] w-[90%] lg:w-[50%] drop-shadow-xl'>
+                    {data?.description}
+                </p>
+                <div className='flex flex-row items-center mt-3 md:mt-4 gap-3'>
+                    <button className='bg-white
+                    text-white bg-opacity-30 rounded py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg: text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition'>{/*Add more info icon mr-1 */}More Info</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BillBoard;
